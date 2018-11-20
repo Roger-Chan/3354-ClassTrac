9950fd689cb8783cf6aa300c5401af4f1f8272e4
dangla1603 <43182305+dangla1603@users.noreply.github.com>
2018-11-20 11:30:30 -0600

    Add files via upload
    updated the listview for StudentPage and ProfessorPage

--- FastTrac/app/src/main/java/utdallas/students/fasttrac/ProfessorPage.java
+++ FastTrac/app/src/main/java/utdallas/students/fasttrac/ProfessorPage.java
@@ -2,12 +2,28 @@ package utdallas.students.fasttrac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ProfessorPage extends AppCompatActivity {
+
+    ListView Listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor_page);
+
+
+        Listview = (ListView)findViewById(R.id.mylistview);
+
+
+        String[] my_class_list = new String []{ "Class 1","Class 2","Class 4","Class 5"};
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1,my_class_list);
+
+        Listview.setAdapter(adapter);
     }
 }
--- FastTrac/app/src/main/java/utdallas/students/fasttrac/StudentPage.java
+++ FastTrac/app/src/main/java/utdallas/students/fasttrac/StudentPage.java
@@ -2,12 +2,32 @@ package utdallas.students.fasttrac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StudentPage extends AppCompatActivity {
+
+
+    ListView Listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_page);
+
+        // Create a Listview object
+        Listview = (ListView)findViewById(R.id.mylistview);
+
+        //defind array values to show into Listview
+        String[] my_class_list = new String []{ "Class 1","Class 2","Class 4","Class 5"};
+
+        //Define an Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1,my_class_list);
+        //Define parameters for the Adapter, a)Context , b) layout for the rows of list
+        // c) ID for TextView to which data is written, d) array of data
+        Listview.setAdapter(adapter);
+
+
     }
 }
