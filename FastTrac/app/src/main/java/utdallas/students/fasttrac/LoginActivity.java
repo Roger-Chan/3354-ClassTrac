3d64d1e9cef05e46a22b6f6ce63e1d9bf2053f9e
Roger-Chan <42976752+Roger-Chan@users.noreply.github.com>
2018-11-27 23:54:45 -0600

    code refactor
    extract method for login screen

--- FastTrac/app/src/main/java/utdallas/students/fasttrac/LoginActivity.java
+++ FastTrac/app/src/main/java/utdallas/students/fasttrac/LoginActivity.java
@@ -12,6 +12,9 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     DatabaseHelper db;
+    Button bLoginbtn;
+    EditText user_name, pass_wrd;
+    TextView invalid, tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,42 +24,52 @@ public class LoginActivity extends AppCompatActivity {
         db = new DatabaseHelper(this);
 
         Button bLoginbtn = (Button)findViewById(R.id.bLogin);
-        final EditText user_name = (EditText)findViewById(R.id.etUsername);
-        final EditText pass_wrd = (EditText)findViewById(R.id.etPassword);
-        final TextView invalid = (TextView) findViewById(R.id.invalid_login);
-        final TextView tvRegister = (TextView) findViewById(R.id.tvRegister);
+        EditText user_name = (EditText)findViewById(R.id.etUsername);
+        EditText pass_wrd = (EditText)findViewById(R.id.etPassword);
+        TextView invalid = (TextView) findViewById(R.id.invalid_login);
+        TextView tvRegister = (TextView) findViewById(R.id.tvRegister);
 
+        logIn();
+
+        Register();
+    }
+
+
+    public void logIn() {
         bLoginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // get the strings from the field
                 String un = user_name.getText().toString();
                 String pwd = pass_wrd.getText().toString();
-                User user = db.validCredentials(un,pwd); // initially error code
+                User user = db.validCredentials(un, pwd); // initially error code
                 Intent secondActivity;
 
                 //student is 0, professor =1
-                if (user == null){
+                if (user == null) {
                     invalid.setVisibility(View.VISIBLE);
-                }   else if(user.authorization == 0) {
+                } else if (user.authorization == 0) {
                     secondActivity = new Intent(getApplicationContext(), StudentPage.class);
-                    secondActivity.putExtra("Student",user);
+                    secondActivity.putExtra("Student", user);
                     startActivity(secondActivity);
-                }   else if(user.authorization == 1){
+                } else if (user.authorization == 1) {
                     secondActivity = new Intent(getApplicationContext(), ProfessorPage.class);
                     secondActivity.putExtra("Professor", user);
                     startActivity(secondActivity);
                 }
             }
         });
+    }
 
+
+    public void Register(){
         tvRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
-
             }
         });
     }
+
 }
