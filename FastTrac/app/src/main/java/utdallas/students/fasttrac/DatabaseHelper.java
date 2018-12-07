9ffab680c9be46ef535f083bb3c8687b831ed88c
gildarts99 <hicks.matthew99@hotmail.com>
2018-12-07 11:25:13 -0600

    Revert "made database items permenant"
    This reverts commit 15d15ebcbd9bb566afa6f2de7800b9f970c19667.

--- FastTrac/app/src/main/java/utdallas/students/fasttrac/DatabaseHelper.java
+++ FastTrac/app/src/main/java/utdallas/students/fasttrac/DatabaseHelper.java
@@ -38,6 +38,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private DatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        deleteAll();
+        //students
+        addUser(new User("username","password","matthew","hicks", "mgh160030@utdallas.edu", 0 ));
+        addUser(new User("mgh160031", "pass", "kevin", "smith", "smith1@utdallas.edu", 0));
+        //professors
+        addUser(new User("professor1", "password", "jason", "smith", "jason.smith@utdallas.edu", 1));
     }
 
     public static DatabaseHelper getInstance(Context context){
