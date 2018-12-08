d56b0fe6b465c3eabc14649902a1618f92432762
dangla1603 <36832407+dml170030@users.noreply.github.com>
2018-12-07 21:35:58 -0600

    Done with UserTest ! Trying to finish loginActivity Test

--- FastTrac/app/src/androidTest/java/utdallas/students/fasttrac/UserTest.java
+++ FastTrac/app/src/androidTest/java/utdallas/students/fasttrac/UserTest.java
@@ -1,20 +1,51 @@
 package utdallas.students.fasttrac;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class UserTest {
 
+    User tester;
+
+
+    @Before
+    public void setup(){
+        tester = new User("name","password","firstname","lastname","name@email.com",0);
+    }
+
     @Test
 
     public void AuthorizationStudent(){
+        //User tUser = new User("name","password","firstname","lastname","name@email.com",0);
 
-        User tUser = new User("name","password","firstname","lastname","name@email.com",0);
-
-        int result = tUser.getAuthorization();
+        int result = tester.getAuthorization();
         assertEquals("student",0,result);
     }
+
+    @Test
+
+    public void getFirstName(){
+
+        String result = tester.getFirst_name();
+        assertEquals("Correct Fist Name","firstname",result);
+    }
+    @Test
+
+    public void getLastName(){
+
+        String result = tester.getLast_name();
+        assertEquals("Correct Last Name","lastname",result);
+    }
+
+    @Test
+    public void getEmail(){
+
+        String result = tester.getEmail();
+        assertEquals("Correct Email","name@email.com",result);
+    }
+
 
 
 }
